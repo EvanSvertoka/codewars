8694ea98e58b620888582d963095fb0ac8d66555
Evan Svertoka <131455378+EvanSvertoka@users.noreply.github.com>
2023-05-14 11:31:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,27 @@
 // 0 <= h <= 23
 // 0 <= m <= 59
 // 0 <= s <= 59
+
+function past(h, m, s) {
+  let milliseconds = 0;
+
+  milliseconds += h * 60 * 60 * 1000;
+  milliseconds += m * 60 * 1000;
+  milliseconds += s * 1000;
+  return milliseconds;
+}
+
+console.log(past(0, 1, 1));
+
+//---
+
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+
